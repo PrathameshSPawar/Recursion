4915a9b7ff5a1a020313d38ac5035d936721db92
Prathamesh Pawar <163292716+prathamesh2124@users.noreply.github.com>
2024-08-02 21:55:13 +0530

    Create FirstUppChar.java

---
+++ FirstUppChar.java
@@ -0,0 +1,11 @@
+public static char firstUP(String str)
+	{
+		for(int i=0;i<str.length();i++)
+		{
+			if(Character.isUpperCase(str.charAt(i)))
+			{
+				return str.charAt(i);
+			}
+		}
+		return 0;
+	}
