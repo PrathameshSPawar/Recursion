d273776ef1b274f2292c6eb3cff425c95679be90
Prathamesh Pawar <163292716+prathamesh2124@users.noreply.github.com>
2024-08-02 21:52:43 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,3 +1,22 @@
 public class BinarySea{
+  public static int binarySer(int arr[],int ele,int s,int e)
+	{
+		if(s>e)
+		{
+			return -1;
+		}
+		int mid=s+(e-s)/2;
+		if(arr[mid]==ele)
+		{
+			return mid;
+		}
 
+		if(arr[mid]>ele)
+		{
+			return binarySer(arr, ele, s, mid-1);
+		}
+		else{
+			return binarySer(arr, ele, mid+1, e);
+		}
+	}
 }
