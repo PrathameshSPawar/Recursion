b71e9e305b6d7cdc04df00858425f42e66afc2be
Prathamesh Pawar <163292716+PrathameshSPawar@users.noreply.github.com>
2024-08-05 20:56:05 +0530

    Create occurance.java

---
+++ occurance.java
@@ -0,0 +1,26 @@
+public static int first=-1;
+    public static int last=-1;
+
+    public static void occurance(String str,int indx,char element)
+    {
+        if(indx==str.length())
+        {
+            System.out.println(first+1);
+            System.out.println(last+1);
+            return;
+        }
+        char curr=str.charAt(indx);
+        if(curr==element)
+        {
+            if(first==-1)
+            {
+                first=indx;
+            }
+            else
+            {
+                last=indx;
+            }
+            //occurance(str, indx+1, element);
+        }
+        occurance(str, indx+1, element);
+    }
